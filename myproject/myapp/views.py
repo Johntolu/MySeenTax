3c2d0ceef8016db84b2d8deea3914104f89dc86d
Tolu <toluadedoja@gmail.com>
2023-05-08 15:56:21 -0700

    deleted myproject

--- myproject/myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from myapp.models import JobListing
-
-def job_list(request):
-    jobs = JobListing.objects.all()
-    return render(request, 'job_list.html', {'jobs': jobs})
